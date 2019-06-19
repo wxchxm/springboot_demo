4fb9bfb9fe9d600e087b8c70496de6970dea3fd5
xmcc <xmcc@163.com>
2019-06-19 19:28:56 +0800

    第一次提交

---
+++ src/main/java/com/wxc/SpringbootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.wxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
